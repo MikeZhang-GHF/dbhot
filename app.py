cf4c38a7857850ced8e206b4330a692ff17910c8
Mike Zhang <zhixiang.zhang2011@gmail.com>
2023-01-14 11:01:46 -0700

    Add binary to app at office

--- app.py
+++ app.py
@@ -2,7 +2,15 @@
 # Developement at office and forget to push to github
 from typing import List 
 
-def binary_search(num: List, target: int) -> int:
+def binary_search(nums: List, target: int) -> int:
     # open range
+    left, right = -1, len(nums) + 1
+    while left + 1 < right:
+        mid = left + right >> 1
+        if nums[mid] < target:
+            left = mid 
+        else:
+            right = mid 
+    return left 
 
 # Dev app function at home
